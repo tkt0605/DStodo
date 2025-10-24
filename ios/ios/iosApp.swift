1afc7343dc4a6c52f196a492495192a0bc921200
tkt0605 <takato.komada2005@outlook.jp>
2025-10-24 23:16:13 +0900

    first commit

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,21 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by 駒田隆人 on 2025/10/24.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct iosApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
